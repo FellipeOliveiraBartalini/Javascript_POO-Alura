7759bf23adca5163a59af8e4f3fc66312de5f207
FellipeOliveiraBartalini <fellipe.oliveira.work@gmail.com>
2022-10-27 16:12:29 -0300

    Criado classe Docente

--- Admin.js
+++ Admin.js
@@ -4,8 +4,8 @@ class Admin extends User {
     constructor(nome, email, nascimento, role = 'admin', ativo = true) {
         super(nome, email, nascimento, role, ativo);
     }
-}
 
-const novoAdmin = new Admin('Fellipe', 'f@f.com', '2021-01-01');
-console.log(novoAdmin);
-console.log(novoAdmin.exibirInfos());
+    criarCurso(nomeDoCurso, vagas) {
+        return `Curso de "${nomeDoCurso}" criado com ${vagas} vagas!`;
+    }
+}
