f62597059dd1665fb6a653c37a3bea286045ddab
FellipeOliveiraBartalini <fellipe.oliveira.work@gmail.com>
2022-10-27 16:00:16 -0300

    Criado classe Admin

--- User.js
+++ User.js
@@ -1,17 +1,13 @@
-class User {
+export default class User {
     constructor(nome, email, nascimento, role, ativo = true) {
-        this.nome = nome;
-        this.email = email;
-        this.nascimento = nascimento;
-        this.role = role || 'estudante';
-        this.ativo = ativo;
+        this.nome = nome
+        this.email = email
+        this.nascimento = nascimento
+        this.role = role || 'estudante'
+        this.ativo = ativo
     }
-
+   
     exibirInfos() {
-        return `${this.nome}, ${this.email}`;
+        return `${this.nome}, ${this.email}`
     }
 }
-
-const novoUser = new User('Fellipe', 'f@f.com', '2021-01-01');
-console.log(novoUser);
-console.log(novoUser.exibirInfos())
