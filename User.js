bc2a348e3c18d07765e881437445e651a802072b
FellipeOliveiraBartalini <fellipe.oliveira.work@gmail.com>
2022-10-28 10:59:21 -0300

    Adicionado Getters and Setters no User

--- User.js
+++ User.js
@@ -1,13 +1,62 @@
 export default class User {
+    #nome;
+    #email;
+    #nascimento;
+    #role;
+    #ativo;
+
     constructor(nome, email, nascimento, role, ativo = true) {
-        this.nome = nome
-        this.email = email
-        this.nascimento = nascimento
-        this.role = role || 'estudante'
-        this.ativo = ativo
+        this.#nome = nome
+        this.#email = email
+        this.#nascimento = nascimento
+        this.#role = role || 'estudante'
+        this.#ativo = ativo
     }
-   
+
+    get nome() {
+        return this.#nome;
+    }
+
+    get email() {
+        return this.#email;
+    }
+
+    get nascimento() {
+        return this.#nascimento;
+    }
+
+    get role() {
+        return this.#role;
+    }
+
+    get ativo() {
+        return this.#ativo;
+    }
+
+    set nome(nome) {
+        if (nome === '') {
+            throw new Error('Nome não pode ser vazio!');
+        }
+        this.#nome = nome;
+    }
+
+    set email(email) {
+        this.#email = email;
+    }
+
+    set nascimento(nascimento) {
+        this.#nascimento = nascimento;
+    }
+
+    set role(role) {
+        this.#role = role;
+    }
+
+    set ativo(ativo) {
+        this.#ativo = ativo;
+    }
+
     exibirInfos() {
-        return `${this.nome}, ${this.email}`
+        return `${this.nome}, ${this.email}, ${this.nascimento}, ${this.role}, ${this.ativo}`;
     }
 }
--- index.js
+++ index.js
@@ -2,5 +2,11 @@ import User from "./User.js";
 import Admin from "./Admin.js";
 import Docente from "./Docente.js";
 
-const novoUser = new User('Fellipe', 'f@f.com', '2021-01-01');
+const novoUser = new User('User', 'u@u.com', '2021-01-01');
 console.log(novoUser.exibirInfos());
+
+const novoAdmin = new Admin('Admin', 'a@a.com', '2021-01-01');
+console.log(novoAdmin.exibirInfos());
+
+novoAdmin.nome = '';
+console.log(novoAdmin.nome)
